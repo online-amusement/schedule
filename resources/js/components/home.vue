34527c204b0617124967ae32a3c9ca0e6ad54a85
olp <t.miyai0001@gmail.com>
2023-10-30 09:07:56 +0900

    laravel立上げ、vuewo

---
+++ resources/js/components/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="app">
+        testArea
+    </div>
+</template>
+<script setup lang="ts">
+import { Component, ref } from 'vue';
+
+console.log("call Home Setup")
+
+</script>
+<style>
+
+</style>
